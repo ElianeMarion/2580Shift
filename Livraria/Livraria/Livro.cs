cf77ea712d92c3f0e8ab6d3138c9ad20203d9f4a
Eliane <eliane@fiap.com.br>
2024-08-06 22:00:42 -0300

    Aula 2: Construtor, encapsulamento

---
+++ Livraria/Livraria/Livro.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Livraria
+{
+	public class Livro
+	{
+		//Padrão: CamelCase - NotaFiscal - ContaCorrente - PessoaFisica
+		//Descrever a classe
+		//Atributos: modificadorAcesso  tipo    nomeAtributo;
+		//				+ -> public     string   nome
+
+		//Encapsulamento é realizado utilizando o modificador de acesso private
+		private string _nome;
+		public string Nome {  
+			get
+			{
+				return _nome;
+			}
+			set
+			{ 
+				if (!value.Equals("")) {
+					_nome = value;
+				}	
+			}
+		}
+		public string Resumo { get; set; }
+		
+		private double valor;
+		public double Valor { get { return valor; } set { valor = value; } }
+
+		private int paginas;
+		public int Paginas { get { return paginas; } set { paginas = value; } }
+		
+		public string Autor {get; set; }
+		
+		private string idioma;
+		//Métodos de acesso
+		//Get
+		public string GetIdioma() { return idioma; }
+		//Set
+		public void SetIdioma(string idioma) {
+			this.idioma = idioma;
+		}
+
+        public Editora Editora { get; set; }
+        public void ExibirDados()
+		{
+			string titulo = "\nDETALHES DO LIVRO";
+			Console.WriteLine(titulo);
+			Console.WriteLine("Nome: " + _nome);
+			Console.WriteLine("Resumo:" + Resumo);
+			Console.WriteLine("Valor: " + Valor);
+			Console.WriteLine("Páginas: " + Paginas);
+
+			if (TemEditora())
+			{
+				Editora.ExibirDados();
+			}
+		}
+
+		public void ExibirDados(string autor)
+		{
+			string titulo = "\nDETALHES DO LIVRO";
+			Console.WriteLine(titulo);
+			Console.WriteLine("Nome: " + _nome);
+			Console.WriteLine("Autor: " + autor);
+			Console.WriteLine("Resumo:" + Resumo);
+			Console.WriteLine("Valor: " + Valor);
+			Console.WriteLine("Páginas: " + Paginas);
+
+			if (TemEditora())
+			{
+				Editora.ExibirDados();
+			}
+		}
+
+		public bool TemEditora()
+		{
+			return Editora != null;
+		}
+
+		
+		//Método Construtor
+		public Livro(string nome, string resumo)
+		{
+			_nome = nome;
+			Resumo = resumo;
+		}
+
+		public Livro(string nome)
+		{
+			_nome = nome;
+	
+		}
+
+		public Livro(string nome, string resumo, double valor, int paginas,  string autor, string idioma, Editora editora) : this(nome)
+		{
+			Nome = nome;
+			Resumo = resumo;
+			this.valor = valor;
+			Valor = valor;
+			this.paginas = paginas;
+			Paginas = paginas;
+			Autor = autor;
+			this.idioma = idioma;
+			Editora = editora;
+		}
+
+	
+
+		public Livro(string nome, Editora editora)
+		{
+			_nome = nome;
+			Editora = editora;
+		}
+		public Livro() { }
+        //Sobrecarga de métodos -> um método com mesmo nome porém com assinaturas diferentes
+    }
+}
---
+++ Livraria/Livraria/Program.cs
@@ -0,0 +1,35 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Livraria;
+
+
+//Instanciar um objeto do tipo Editora
+Editora novaEditora = new Editora();
+novaEditora.Nome = "Novatec";
+novaEditora.Telefone = "1129875485";
+novaEditora.Site = "www.novatec.com.br";
+
+//Console.WriteLine("Editora: " + novaEditora.Nome);
+//Instanciar um objeto -> Criar o objeto na memória
+Livro meuLivro = new Livro("Harry Potter", novaEditora);
+//Console.WriteLine(meuLivro.Nome);
+meuLivro.Paginas = 150;
+meuLivro.Valor = 100;
+meuLivro.Resumo = "Harry Potter é um bruxo ...";
+meuLivro.Autor = "Autor x";
+//meuLivro.Nome = "Harry Potter";
+meuLivro.SetIdioma("Ingles");
+//meuLivro.Editora = novaEditora;
+meuLivro.ExibirDados();
+//Console.WriteLine(meuLivro.Nome + " foi publicado pela editora: " + meuLivro.Editora.Nome);
+
+Livro livroUML = new Livro("UML2");
+//livroUML.Nome = "UML2";
+livroUML.Autor = "Gilleanes Guedes";
+livroUML.Paginas = 485;
+livroUML.Valor = 100;
+livroUML.Resumo = "UML 2.0 uma abordagem prática";
+//livroUML.Idioma = "Portugues";
+livroUML.ExibirDados();
+
+
+Editora erika = new Editora();
